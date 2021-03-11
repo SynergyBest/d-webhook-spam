6aef033b34c0a5839419b5de92041b3220d59ba9
theo <72149158+SynergyBest@users.noreply.github.com>
2021-03-11 10:30:18 -0600

    Made the script more efficient
    It was defining the variable data every time which was unnecessary

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import requests
 import time
 
-while True:
-  webhook = # discord webhook url
-    }
+webhook = ""  # webhook url
 
-  data = {
-      "username": "botname", # bot username
-      "avatar_url": "" # link to image of the webhook
-      "content": "<@!userid>", # id of the user to spam
-      "embeds": [
-      {
-        "title": "", # embed title
-        "url": "", # link in the embed title
-        "color": "", # color of embed
-        "description": "", # webhook description
-        "image": {
-          "url": "" # image url
+data = {
+    "username": "botname",  # bot username
+    "avatar_url": "",  # link to image of the webhook
+    "content": "<@!userid>",  # id of the user to spam
+    "embeds": [
+        {
+            "title": "",  # embed title
+            "url": "",  # link in the embed title
+            "color": "",  # color of embed
+            "description": "",  # webhook description
+            "image": {"url": ""},  # image url
         }
-      }]
-    }
+    ],
+}
 # All params: https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-structure
-  result = requests.post(webhook, json=data)
-  if 200 <= result.status_code < 300:
-      print(f"Webhook sent with status {result.status_code}")
-  else:
-      print(f"Webhook errored with status {result.status_code}, response:\n{result.json()}")
-  time.sleep(1)
+while True:
+    result = requests.post(webhook, json=data)
+    if 200 <= result.status_code < 300:
+        print(
+          f"Webhook sent with status {result.status_code}"
+        )
+    else:
+        print(
+            f"Webhook errored with status {result.status_code}, response:\n{result.json()}"
+        )
+    time.sleep(5) # seconds to wait inbetween webhooks (otherwise you get ratelimited)
